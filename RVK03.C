bcc0268b72039f1383a3e48d11c0f00456704b87
rajveer00007 <rajveerkanojjiya007@gmail.com>
2026-08-31 10:49:44 +0530

    Create RVK03.C

---
+++ RVK03.C
@@ -0,0 +1,14 @@
+/*
+Program(3)-Write a program to calculate teh area of a circle
+Solution(1)-Predefined Value
+*/
+
+#include<stdio.h>
+int main()
+{
+    float radius=1.1;
+    float area;
+    area=(3.14*radius*radius);
+    printf("\n Area of Circle=%f",area);
+}
+    
